4d2722c4ef7f7c86f56d813a5f7623aa3591f3b8
Roozbeh Ketabi <roozbehketabi@gmail.com>
2013-05-21 15:02:02 +0430

    added .css and .js

--- src/Roozbeh/NewsShowBundle/Controller/DefaultController.php
+++ src/Roozbeh/NewsShowBundle/Controller/DefaultController.php
@@ -4,6 +4,25 @@ namespace Roozbeh\NewsShowBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+class newsSummary
+{
+    public $title;
+    public $author;
+    public $summary;
+    public $time;
+    public $groupName;
+    public $ImgSrc;
+    function __construct($t,$a,$s,$ti,$gn,$src)
+    {
+        $this->title=$t;
+        $this->author=$a;
+        $this->summary=$s;
+        $this->time=$ti;
+        $this->groupName=$gn;
+        $this->ImgSrc=$src;
+    }
+}
+
 class DefaultController extends Controller
 {
     public function indexAction()
@@ -14,7 +33,11 @@ class DefaultController extends Controller
 
     public function newsAction()
     {
-        $arr = array('mehdi' => 12,'rzb'=> 34);
-        return $this->render('NewsShowBundle:Default:news.html.twig', $arr);
+        $items[] = new newsSummary('test1','roozbeh1','charand e mozakhraf 1','9:30 1' , 'siasi 1' , 'bundles/newsshow/images/jafang.jpg');
+        $items[] = new newsSummary('test2','roozbeh2','charand e mozakhraf 2','9:30 2' , 'siasi 2' , 'bundles/newsshow/images/jafang.jpg');
+        $items[] = new newsSummary('test3','roozbeh3','charand e mozakhraf 3','9:30 3' , 'siasi 3' , 'bundles/newsshow/images/jafang.jpg');
+        $arr = array('newsSumItems' => $items );
+
+        return $this->render('NewsShowBundle:Default:news.html.twig',$arr);
     }
 }
